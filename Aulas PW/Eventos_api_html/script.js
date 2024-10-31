7ae708fce10bfb928315004bdd3286c107ba26f6
Notbook Gabriel <gabrielhma98@gmail.com>
2024-10-31 10:52:36 -0300

    continuação do projeto de python da aula PW

--- Aulas PW/Eventos_api_html/script.js
+++
@@ -1,8 +0,0 @@
-let nome = "Gabriel Henrique"
-
-document.getElementById('clicavel').addEventListener('click', function(){alert('Botão foi clicado')})
-
-function clicar(){
-    //alert('Clique Novo')
-    document.getElementById("titulo").innerHTML = `seja bem vindo ${nome}`;
-}
